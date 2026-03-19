8d013f15d37041267b9c11f0636e60adb630ad94
Christos Karydis <karydes.c@icloud.com>
2026-03-20 00:40:46 +0200

    Initial commit: Project Scaffolding

---
+++ backend/src/main/java/com/github/karydesc/ttsreader/TtsreaderApplication.java
@@ -0,0 +1,13 @@
+package com.github.karydesc.ttsreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtsreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtsreaderApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/karydesc/ttsreader/TtsreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.karydesc.ttsreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtsreaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
